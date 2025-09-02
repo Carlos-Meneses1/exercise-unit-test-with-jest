2c1a504dc482f66268d81644f40e0a3deecfd8f7
Carlos-Meneses1 <carlosocharly1@hotmail.com>
2025-09-02 01:40:12 +0000

    add test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-// test.js
+// Importacion de las funciones
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
-// Importamos la función desde app.js
-const { fromEuroToDollar } = require('./app.js');
+/////////////////////prueba EUR > USD//////////////////////
 
 test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
@@ -16,3 +16,23 @@ test("One euro should be 1.07 dollars", function() {
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
+
+
+////////////////////// prueba USD > JPY////////////////////////
+test("One dollar should be 146.26 yens", function() {
+    // Transformar de dolares a euros: 1 ÷ 1.07
+    // Luego transformar de euros a yenes: EUR × 156.5
+    const yens = fromDollarToYen(1);
+    //regla de 3
+    const expected = (1 / 1.07) * 156.5;
+    expect(fromDollarToYen(1)).toBe(146.26168224299064); 
+})
+
+
+
+
+
+
+
+
+
